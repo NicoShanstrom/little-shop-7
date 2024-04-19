56b78866f15067271748d788c1e487250cca7fcd
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-04-19 12:00:39 -0400

    Add validations for coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,10 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices
   has_many :customers, through: :invoices
+
+  validates :name, presence: true
+  validates :code, presence: true
+  validates :discount_amount, presence: true
+  validates :percent_off, presence: true
+  validates :status, presence: true
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
+  before :each do
+    @merchant1 = Merchant.create!(name: "The best merchant")
+  end
   describe 'relationships' do
     it { should belong_to :merchant }
     it { should have_many :invoices }
     it { should have_many(:customers).through(:invoices) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:discount_amount) }
+    it { should validate_presence_of(:percent_off) }
+    it { should validate_presence_of(:status) }
   end
 end
